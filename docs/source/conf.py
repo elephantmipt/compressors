817a921a1fa280f69d74b5c33b189c81b09fd5ee
Nikita <Balaganskij.NN@phystech.edu>
2021-03-08 17:08:08 +0300

    fix name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Caompressors'
+project = 'Compressors'
 copyright = '2021, Nikita Balagansky'
 author = 'Nikita Balagansky'
 
