079a84c812c65622e311bb4d4df9c820c338bd1e
Nikita <Balaganskij.NN@phystech.edu>
2021-03-16 23:33:03 +0300

    fix imports

--- compressors/distillation/student_init/__init__.py
+++ compressors/distillation/student_init/__init__.py
@@ -1,3 +1,7 @@
-from .bert import init_bert_model_with_teacher
+from compressors.settings import IS_TRANSFORMERS_AVAILABLE
 
-__all__ = ["init_bert_model_with_teacher"]
+
+if IS_TRANSFORMERS_AVAILABLE:
+    from .bert import init_bert_model_with_teacher
+
+    __all__ = ["init_bert_model_with_teacher"]
