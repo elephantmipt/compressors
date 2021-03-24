8aa8e3b3392e0a68328648e56c3dc3b29e45be05
Nikita <Balaganskij.NN@phystech.edu>
2021-03-25 00:31:05 +0300

    fix

--- compressors/distillation/callbacks/logits_diff/kl_div_callback.py
+++ compressors/distillation/callbacks/logits_diff/kl_div_callback.py
@@ -1,16 +1,18 @@
-from compressors.distillation.losses import kl_div_loss
+from compressors.distillation.losses import KLDivLoss
 from compressors.distillation.callbacks.order import CallbackOrder
 from catalyst.core import Callback
 
 
 class KLDivCallback(Callback):
-    def __init__(self, output_key: str = "kl_div_loss"):
+    def __init__(self, output_key: str = "kl_div_loss", temperature: float = 1.):
         super().__init__(order=CallbackOrder.Metric)
         self.output_key = output_key
+        self.criterion = KLDivLoss(temperature=temperature)
 
     def on_batch_end(self, runner):
-        runner.batch_metrics[self.output_key] = kl_div_loss(
-            s_logits=runner.batch["s_logits"], t_logits=runner.batch["t_logits"],
+        runner.batch_metrics[self.output_key] = self.criterion(
+            s_logits=runner.batch["s_logits"],
+            t_logits=runner.batch["t_logits"],
         )
 
 
