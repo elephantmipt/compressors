ff3d24843e0dff3349421866ef9b858f75fb3d8c
Nikita <Balaganskij.NN@phystech.edu>
2021-03-24 14:47:45 +0300

    add preact models for CIFAR

--- compressors/models/cv/resnet_cifar.py
+++ compressors/models/cv/resnet_cifar.py
@@ -1,7 +1,10 @@
+from typing import Union, Tuple, Dict
+
 from torch import nn
 from torch import FloatTensor
 
 from .resnet_modules import BasicBlock, Bottleneck
+from .preact_modules import PreActBlock, PreActBottleneck
 
 from compressors.models import BaseDistilModel
 
@@ -12,19 +15,31 @@ class ResNetCifar(BaseDistilModel):
         # Model type specifies number of layers for CIFAR-10 model
         if block_name.lower() == "basicblock":
             assert (
-                depth - 2
-            ) % 6 == 0, "When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202"
+                           depth - 2
+                   ) % 6 == 0, "When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202"
             n = (depth - 2) // 6
             block = BasicBlock
         elif block_name.lower() == "bottleneck":
             assert (
-                depth - 2
-            ) % 9 == 0, "When use bottleneck, depth should be 9n+2, e.g. 20, 29, 47, 56, 110, 1199"
+                           depth - 2
+                   ) % 9 == 0, "When use bottleneck, depth should be 9n+2, e.g. 20, 29, 47, 56, 110, 1199"
             n = (depth - 2) // 9
             block = Bottleneck
+        elif block_name.lower() == "preactblock":
+            assert (
+                           depth - 2
+                   ) % 6 == 0, "When use basicblock, depth should be 6n+2, e.g. 20, 32, 44, 56, 110, 1202"
+            n = (depth - 2) // 6
+            block = PreActBlock
+        elif block_name.lower() == "preactbottleneck":
+            assert (
+                           depth - 2
+                   ) % 9 == 0, "When use bottleneck, depth should be 9n+2, e.g. 20, 29, 47, 56, 110, 1199"
+            n = (depth - 2) // 9
+            block = PreActBottleneck
         else:
             raise ValueError("block_name shoule be Basicblock or Bottleneck")
-
+        self.block_name = block_name
         self.inplanes = num_filters[0]
         self.conv1 = nn.Conv2d(3, num_filters[0], kernel_size=3, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_filters[0])
@@ -64,49 +79,35 @@ class ResNetCifar(BaseDistilModel):
 
         return nn.Sequential(*layers)
 
-    def get_feat_modules(self):
-        feat_m = nn.ModuleList([])
-        feat_m.append(self.conv1)
-        feat_m.append(self.bn1)
-        feat_m.append(self.relu)
-        feat_m.append(self.layer1)
-        feat_m.append(self.layer2)
-        feat_m.append(self.layer3)
-        return feat_m
-
-    def get_bn_before_relu(self):
-        if isinstance(self.layer1[0], Bottleneck):
-            bn1 = self.layer1[-1].bn3
-            bn2 = self.layer2[-1].bn3
-            bn3 = self.layer3[-1].bn3
-        elif isinstance(self.layer1[0], BasicBlock):
-            bn1 = self.layer1[-1].bn2
-            bn2 = self.layer2[-1].bn2
-            bn3 = self.layer3[-1].bn2
-        else:
-            raise NotImplementedError("ResNet unknown block error !!!")
-
-        return [bn1, bn2, bn3]
-
     def forward(
-        self,
-        x: FloatTensor,
-        output_hidden_states: bool = False,
-        return_dict: bool = False,
-        preact: bool = False,
-    ) -> FloatTensor:
+            self,
+            x: FloatTensor,
+            output_hidden_states: bool = False,
+            return_dict: bool = False,
+            preact: bool = False,
+    ) -> Union[FloatTensor, Tuple[FloatTensor], Dict[str, FloatTensor]]:
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)  # 32x32
         f0 = x
-
-        x, f1_pre = self.layer1(x)  # 32x32
+        if preact and "preact" in self.block_name:
+            raise Exception("PreAct blocks doesn't have activation on last layer.")
+        if "preact" not in self.block_name:
+            x, f1_pre = self.layer1(x)  # 32x32
+        else:
+            x = self.layer1(x)
         f1 = x
-        x, f2_pre = self.layer2(x)  # 16x16
+        if "preact" not in self.block_name:
+            x, f2_pre = self.layer2(x)  # 16x16
+        else:
+            x = self.layer2(x)
         f2 = x
-        x, f3_pre = self.layer3(x)  # 8x8
+        if "preact" not in self.block_name:
+            x, f3_pre = self.layer3(x)  # 8x8
+        else:
+            x = self.layer3(x)
         f3 = x
-
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         f4 = x
@@ -157,3 +158,39 @@ def resnet_cifar_8x4(**kwargs):
 
 def resnet_cifar_32x4(**kwargs):
     return ResNetCifar(32, [32, 64, 128, 256], "basicblock", **kwargs)
+
+
+def preact_resnet_cifar_8(**kwargs):
+    return ResNetCifar(8, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_14(**kwargs):
+    return ResNetCifar(14, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_20(**kwargs):
+    return ResNetCifar(20, [16, 16, 32, 64], "preaactblock", **kwargs)
+
+
+def preact_resnet_cifar_32(**kwargs):
+    return ResNetCifar(32, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_44(**kwargs):
+    return ResNetCifar(44, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_56(**kwargs):
+    return ResNetCifar(56, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_110(**kwargs):
+    return ResNetCifar(110, [16, 16, 32, 64], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_8x4(**kwargs):
+    return ResNetCifar(8, [32, 64, 128, 256], "preactblock", **kwargs)
+
+
+def preact_resnet_cifar_32x4(**kwargs):
+    return ResNetCifar(32, [32, 64, 128, 256], "preactblock", **kwargs)
