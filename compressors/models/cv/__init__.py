0b3f6708563c21c702b64f05475bda8c71b5e778
Nikita <Balaganskij.NN@phystech.edu>
2021-03-24 15:02:31 +0300

    fix cv modules

--- compressors/models/cv/__init__.py
+++ compressors/models/cv/__init__.py
@@ -1,3 +1,15 @@
+from .resnet import (
+    resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
+    resnext50_32x4d,
+    resnext101_32x8d,
+    wide_resnet50_2,
+    wide_resnet101_2,
+)
+
 from .preact_resnet import (
     PreActResNet18,
     PreActResNet34,
@@ -5,12 +17,23 @@ from .preact_resnet import (
     PreActResNet101,
     PreActResNet152,
 )
-
-
-__all__ = [
-    "PreActResNet18",
-    "PreActResNet34",
-    "PreActResNet50",
-    "PreActResNet101",
-    "PreActResNet152",
-]
+from .resnet_cifar import (
+    resnet_cifar_8,
+    resnet_cifar_8x4,
+    resnet_cifar_14,
+    resnet_cifar_20,
+    resnet_cifar_32,
+    resnet_cifar_44,
+    resnet_cifar_56,
+    resnet_cifar_110,
+    resnet_cifar_32x4,
+    preact_resnet_cifar_8,
+    preact_resnet_cifar_8x4,
+    preact_resnet_cifar_14,
+    preact_resnet_cifar_20,
+    preact_resnet_cifar_32,
+    preact_resnet_cifar_32x4,
+    preact_resnet_cifar_44,
+    preact_resnet_cifar_56,
+    preact_resnet_cifar_110,
+)
--- compressors/utils/data/torchvision.py
+++ compressors/utils/data/torchvision.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils import data
 
 
