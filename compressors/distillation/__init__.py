ebcbe9e6dae8ee92cdafd1688542f540aecc26dd
Nikita <Balaganskij.NN@phystech.edu>
2021-03-24 22:46:59 +0300

    fix init

--- compressors/distillation/__init__.py
+++ compressors/distillation/__init__.py
@@ -1,6 +1,5 @@
 from compressors.distillation.hidden_states import hidden_states_select
 from compressors.distillation.runners import HFDistilRunner, DistilRunner
-from compressors.distillation.student_init import init_bert_model_with_teacher
 
 
 __all__ = [
