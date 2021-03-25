304629f4580f188f97618426430d5714b38ed625
Nikita <Balaganskij.NN@phystech.edu>
2021-03-25 10:26:38 +0300

    add probability shift to runner

--- compressors/distillation/runners/distil_runner.py
+++ compressors/distillation/runners/distil_runner.py
@@ -1,11 +1,20 @@
 from catalyst.utils import get_nn_from_ddp_module, set_requires_grad
 from catalyst.runners import Runner
 
+from compressors.distillation.data.label_smoothing import probability_shift
+
 
 class DistilRunner(Runner):
-    def __init__(self, output_hidden_states: bool = True, *runner_args, **runner_kwargs):
+    def __init__(
+        self,
+        output_hidden_states: bool = True,
+        apply_probability_shift: bool = False,
+        *runner_args,
+        **runner_kwargs
+    ):
         super().__init__(*runner_args, **runner_kwargs)
         self.output_hidden_states = output_hidden_states
+        self.apply_probability_shift = apply_probability_shift
 
     def handle_batch(self, batch):
         model = get_nn_from_ddp_module(self.model)
@@ -24,6 +33,10 @@ class DistilRunner(Runner):
         self.batch["s_logits"] = s_outputs["logits"]
         if self.is_train_loader:
             self.batch["t_logits"] = t_outputs["logits"]
+            if self.apply_probability_shift:
+                self.batch["t_logits"] = probability_shift(
+                    logits=self.batch["t_logits"], labels=self.batch["targets"]
+                )
         if self.output_hidden_states:
             self.batch["s_hidden_states"] = s_outputs["hidden_states"]
             if self.is_train_loader:
