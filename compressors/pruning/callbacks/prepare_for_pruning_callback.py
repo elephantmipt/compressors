7dcfcda65603fc93534bbc8e7b53461dd35ff633
Nikita <Balaganskij.NN@phystech.edu>
2021-03-26 18:53:44 +0300

    fix

--- compressors/pruning/callbacks/prepare_for_pruning_callback.py
+++ compressors/pruning/callbacks/prepare_for_pruning_callback.py
@@ -1,3 +1,5 @@
+from torch.utils.data import DataLoader
+
 from catalyst.core import Callback, CallbackOrder, CallbackNode, IRunner
 from compressors.distillation.data import LogitsDataset
 
@@ -14,5 +16,12 @@ class PrepareForFinePruningCallback(Callback):
             return
         train_loader = runner.loaders[self.train_loader_key]
         train_dataset = LogitsDataset(train_loader.dataset, runner.model)
-        train_loader.dataset = train_dataset
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=train_loader.batch_size,
+            sampler=train_loader.sampler,
+            collate_fn=train_loader.collate_fn,
+            num_workers=train_loader.num_workers,
+            drop_last=train_loader.drop_last,
+        )
         runner.loaders[self.train_loader_key] = train_loader
