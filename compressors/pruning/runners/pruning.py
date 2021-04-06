f954d80cf450376f40fdbcb0822ed223a5481476
Nikita <Balaganskij.NN@phystech.edu>
2021-04-06 21:09:25 +0300

    fix runner

--- compressors/pruning/runners/pruning.py
+++ compressors/pruning/runners/pruning.py
@@ -1,11 +1,17 @@
 from typing import Any, Mapping, Union, List
-from collections import OrderedDict
 
-from catalyst.dl import Callback, Runner
+from catalyst.dl import Runner
 
 
 class PruneRunner(Runner):
-    def __init__(self, num_sessions: int = 5, input_key: Union[str, List[str]] = "features", *runner_args, **runner_kwargs):
+    def __init__(
+        self,
+        num_sessions: int = 5,
+        input_key: Union[str, List[str]] = "features",
+        output_key: Union[str, List[str]] = "logits",
+        *runner_args,
+        **runner_kwargs
+    ):
         """
         Base runner for iterative pruning.
         Args:
@@ -14,6 +20,13 @@ class PruneRunner(Runner):
             **runner_kwargs: runner kwargs
         """
         super().__init__(*runner_args, **runner_kwargs)
+        if isinstance(input_key, str):
+            input_key = [input_key]
+        if isinstance(output_key, str):
+            output_key = [output_key]
+
+        self.input_key = input_key
+        self.output_key = output_key
         self._num_sessions = num_sessions
 
     @property
@@ -21,7 +34,10 @@ class PruneRunner(Runner):
         return [f"pruning_session_{i}" for i in range(self._num_sessions)]
 
     def handle_batch(self, batch: Mapping[str, Any]) -> None:
-        self.batch["logits"] = self.model(batch["features"])
+        model_inp = [batch[key] for key in self.input_key]
+        model_out = self.model(*model_inp)
+        for idx, key in enumerate(self.output_key):
+            self.batch[key] = model_out[idx]
 
 
 __all__ = ["PruneRunner"]
